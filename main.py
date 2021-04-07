d9e21a6bd05160f9428789045815fc2bab3c9fde
Deepesh Nimma <deepnimma@gmail.com>
2021-04-07 21:56:22 +0530

    added command line logging

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 from pathlib import Path
 from dotenv import load_dotenv
 
+from datetime import datetime
+
 # Loading Data From .env File
 load_dotenv()
 env_path = Path('.') / '.env'
@@ -17,13 +19,26 @@ client = commands.Bot(command_prefix = '=')
 async def on_ready():
 	print('Bot is Ready')
 
+@client.event
+async def on_message(message):
+    # Only Reads From Private Testing Discord and Math-UwU-and-Nitro-Channel
+    #                              Priv                                           Math UWU
+   
+    # if message.channel.id == '829357837976076351' or message.channel.id == '784543998366842920':
+    current_time = datetime.now()
+    current_time = current_time.strftime("%d/%m/%Y %H:%M:%S") # Get Current Time (For Debugging)
+    print('[%s] - %s sent %s' % (current_time, message.author, message.content))
+
 @client.command()
-async def wehaveawinner():
-    print('NottCurious Wins Everything!!!!!!!!!!!!!!!!!!!!')
+async def wehaveawinner(ctx):
+    await ctx.send('NottCurious Wins Everything!!!!!!!!!!!!!!!!!!!!')
 
 @client.command()
 async def whosadick(ctx):
     await ctx.send('Kelvin\'s a Dick!!')
 
+
 # Execute Commands
 client.run(BOTTOKEN)
+
+log_txt_file.close()
