9f22538599fb6969b7679426d0b0e6dcd547ffcd
Deepesh Nimma <deepnimma@gmail.com>
2021-04-07 19:32:17 +0530

    setting up main source and requirements

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+import os
+from discord.ext import commands
+from pathlib import Path
+from dotenv import load_dotenv
+
+# Loading Data From .env File
+load_dotenv()
+env_path = Path('.') / '.env'
+api_key = os.getenv("API_KEY")
+BOTTOKEN = os.getenv("BOT_TOKEN")
+
+# Setting Up Bot
+client = commands.Bot(command_prefix = '=')
+
+# Printing String When Bot Is Ready To Be Used
+@client.event
+async def on_ready():
+	print('Bot is Ready')
+
+
+@client.command()
+async def whosadick(ctx):
+    await ctx.send('Kelvin\'s a Dick!!')
+
+# Execute Commands
+client.run(BOTTOKEN)
